ef4b7f855e40f1dbe5a5e0c61d1ddf29bfa037dc
vitalyvitmens <otm_@tut.by>
2023-07-03 00:43:10 +0300

    add data in project

--- my-app/src/components/todo-list/todo-list.js
+++ my-app/src/components/todo-list/todo-list.js
@@ -8,10 +8,18 @@ export const TodoList = ({
 	setTodo,
 	setIsUpdating,
 }) => {
+	const ellipsis = (str, n) => {
+		if (str.length <= n) {
+			return str
+		} else {
+			return str.slice(0, n) + '...'
+		}
+	}
+
 	return todosServer.map(({ id, title }) => (
 		<ol key={id}>
 			<span>{id}</span>
-			{title}
+			{ellipsis(title, 25)}
 			<button
 				className={!todo ? styles.updateBtnYellow : styles.updateBtnGreen}
 				onClick={() => {

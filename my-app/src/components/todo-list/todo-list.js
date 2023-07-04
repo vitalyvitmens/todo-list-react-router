2111f3a096217d8ecdfa77ed996b773661794ace
vitalyvitmens <otm_@tut.by>
2023-07-04 16:10:17 +0300

    add title & search toLowerCase()

--- my-app/src/components/todo-list/todo-list.js
+++ my-app/src/components/todo-list/todo-list.js
@@ -17,61 +17,36 @@ export const TodoList = ({
 		}
 	}
 
-	return search
-		? todosServer
-				.filter((todo) => {
-					return search ? todo.title.includes(search) : todo
-				})
-				.map(({ id, title }) => (
-					<ol key={id}>
-						<span>{id}</span>
-            {ellipsis(String(title), 25)}
-						<button
-							className={!todo ? styles.updateBtnYellow : styles.updateBtnGreen}
-							onClick={() => {
-								if (todo === '') {
-									setIsUpdating(true)
-									setTodo(title)
-								} else {
-									requestUpdateTodo(id)
-									setTodo('')
-								}
-							}}
-						>
-							✎
-						</button>
-						<button
-							className={styles.deleteBtn}
-							onClick={() => requestDeleteTodo(id)}
-						>
-							X
-						</button>
-					</ol>
-				))
-		: todosServer.map(({ id, title }) => (
-				<ol key={id}>
-					<span>{id}</span>
-					{ellipsis(String(title), 25)}
-					<button
-						className={!todo ? styles.updateBtnYellow : styles.updateBtnGreen}
-						onClick={() => {
-							if (todo === '') {
-								setIsUpdating(true)
-								setTodo(title)
-							} else {
-								requestUpdateTodo(id)
-								setTodo('')
-							}
-						}}
-					>
-						✎
-					</button>
-					<button
-						className={styles.deleteBtn}
-						onClick={() => requestDeleteTodo(id)}
-					>
-						X
-					</button>
-				</ol>
-		  ))
+	return todosServer
+		.filter((todo) => {
+			return search
+				? todo.title.toLowerCase().includes(search.toLowerCase())
+				: todo
+		})
+		.map(({ id, title }) => (
+			<ol key={id}>
+				<span>{id}</span>
+				{ellipsis(String(title), 25)}
+				<button
+					className={!todo ? styles.updateBtnYellow : styles.updateBtnGreen}
+					onClick={() => {
+						if (todo === '') {
+							setIsUpdating(true)
+							setTodo(title)
+						} else {
+							requestUpdateTodo(id)
+							setTodo('')
+						}
+					}}
+				>
+					✎
+				</button>
+				<button
+					className={styles.deleteBtn}
+					onClick={() => requestDeleteTodo(id)}
+				>
+					X
+				</button>
+			</ol>
+		))
 }

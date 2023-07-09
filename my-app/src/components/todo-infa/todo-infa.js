a6e504153a7087999359a254b3473f428d2c873b
vitalyvitmens <otm_@tut.by>
2023-07-09 17:39:59 +0300

    add data in project

--- my-app/src/components/todo-infa/todo-infa.js
+++ my-app/src/components/todo-infa/todo-infa.js
@@ -16,50 +16,47 @@ export const TodoInfa = ({
 }) => {
 	const navigate = useNavigate()
 
-	const currentTodo = todosServer.filter(
-		(todo) => todo.id === Number(todoData.id)
-	)
-
 	return (
 		<>
 			<div className={styles.btn_h1}>
 				<Button onClick={() => navigate('/')}>На Главную</Button>
 			</div>
-
-			{currentTodo.map(({ id, title, completed }) => (
-				<ol key={id} onClick={onSubmit}>
-					<span>{id}</span>
-					<div
-						className={completed ? styles.todoLineThrough : styles.todo}
-						onClick={() => {
-							toggleCompletedHandler()
-							requestUpdateCompletedTodo(id)
-						}}
-					>
-						{title}
-					</div>
-					<button
-						className={!todo ? styles.updateBtnYellow : styles.updateBtnGreen}
-						onClick={() => {
-							if (todo === '') {
-								setIsUpdating(true)
-								setTodo(title)
-							} else {
-								requestUpdateTodo(id)
-								setTodo('')
-							}
-						}}
-					>
-						✎
-					</button>
-					<button
-						className={styles.deleteBtn}
-						onClick={() => requestDeleteTodo(id)}
-					>
-						X
-					</button>
-				</ol>
-			))}
+			{todosServer
+				.filter((todo) => todo.id === todoData.id)
+				.map(({ id, title, completed }) => (
+					<ol key={id} onClick={onSubmit}>
+						<span>{id}</span>
+						<div
+							className={completed ? styles.todoLineThrough : styles.todo}
+							onClick={() => {
+								toggleCompletedHandler()
+								requestUpdateCompletedTodo(id)
+							}}
+						>
+							{title}
+						</div>
+						<button
+							className={!todo ? styles.updateBtnYellow : styles.updateBtnGreen}
+							onClick={() => {
+								if (todo === '') {
+									setIsUpdating(true)
+									setTodo(title)
+								} else {
+									requestUpdateTodo(id)
+									setTodo('')
+								}
+							}}
+						>
+							✎
+						</button>
+						<button
+							className={styles.deleteBtn}
+							onClick={() => requestDeleteTodo(id)}
+						>
+							X
+						</button>
+					</ol>
+				))}
 		</>
 	)
 }
